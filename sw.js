d0788370b62a4d9747688f02b834d141cbcfa83b
PiaStayAlive <se.ama.0429@gmail.com>
2026-04-09 08:11:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-// Service Workerを無効化してキャッシュをクリアする魔法のコード
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  // 何もしない（キャッシュを使わず、常に最新のネット上の画像を見に行く）
-});
+self.addEventListener('fetch', function(event) {});
